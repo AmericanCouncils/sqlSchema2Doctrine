098b00502577a3280934b5d8bc4b394d5e8d07a4
David Simon <dsimon@americancouncils.org>
2013-11-06 12:04:45 -0500

    Parsing column options

--- sqlSchema2doctrine.py
+++ sqlSchema2doctrine.py
@@ -11,9 +11,22 @@ columnPattern = re.compile(
     r'`(?P<name>\w+)` (?P<type>\S+) (?P<options>.+)'
 )
 
+defaultValuePattern = re.compile(
+    r"\bDEFAULT (?P<value>(?:\w+|'[^']+'))"
+)
+
+notNullPattern = re.compile(
+    r"\bNOT NULL"
+)
+
 sql = open(sys.argv[1], 'r').read()
 for tbl in tablePattern.finditer(sql):
     print tbl.group('name')
     for col in columnPattern.finditer(tbl.group('def')):
         print "N: %s   T: %s" % (col.group('name'), col.group('type'))
+        defaultValue = defaultValuePattern.search(col.group('options'))
+        if defaultValue:
+            print "   DEFAULT %s" % (defaultValue.group('value'))
+        if notNullPattern.search(col.group('options')):
+            print "   NOT NULL"
     print
